aad8234e9c59edfcd68dd074caae5db29952289d
Alexander Neupokoev <admin@armavir.ru>
2022-03-16 14:54:08 +0300

    Add README

--- init.lua
+++ init.lua
@@ -47,4 +47,11 @@ return require('packer').startup(function()
     end
   }
   use 'p00f/nvim-ts-rainbow'
+  use {'iamcco/markdown-preview.nvim',
+    run = 'cd app && npm install',
+    setup = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' }
+  } 
 end)
